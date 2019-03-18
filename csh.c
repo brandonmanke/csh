7742f8864e0416c7650a7a4bc0f7828fce900632
Brandon Manke <manke.brandon@gmail.com>
2019-03-18 00:25:52 -0500

    add: parse cmd, input checks, fix tokenize_str

--- csh.c
+++ csh.c
@@ -1,3 +1,7 @@
+/**
+ * Program 1 - Part 2 : Basic Shell Implementation
+ * @author Brandon Manke
+ */
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
@@ -6,18 +10,26 @@
 #define MAX_LINE 80
 
 typedef struct cmd_t {
-    char* cmd;
+    char*  cmd;
     char** params;
-    int params_size;
+    int    params_size;
 } cmd_t;
 
 // TODO
-void parse_token(cmd_t* c) {
-    printf("cmd: %s\n", c->cmd);
+void parse_cmd(cmd_t* c) {
+    if (c->cmd == NULL || c == NULL) {
+        return;
+    }
+    //printf("cmd: %s\n", c->cmd);
     if (strcmp(c->cmd, "ls") == 0) {
         printf("ls command\n");
     } else {
-        fprintf(stderr, "unrecognized command: %s", c->cmd);
+        fprintf(stderr, "unrecognized command: %s\n", c->cmd);
+        fprintf(stderr, "with params: ");
+        for (int i = 0; i < c->params_size; i++) {
+            fprintf(stderr, "%s ", c->params[i]);
+        }
+        fprintf(stderr, "\n");
     }
 }
 
@@ -29,17 +41,20 @@ cmd_t* tokenize_str(char* fstr) {
     const int MAX_PSIZE = 16;
     char** p = malloc(MAX_PSIZE);
     while(c != NULL) {
-        if (*c != '\n' && count == 0) {
-            command->cmd = c;
-            count++;
+        if (*c != '\n') {
+            if (count == 0) {
+                command->cmd = malloc(strlen(c) + 1);
+                strcpy(command->cmd, c);
+                count++;
+            } else if (count != 0 && pcount < MAX_PSIZE) {
+                p[pcount] = malloc(strlen(c) + 1);
+                p[pcount] = c;
+                pcount++;
+            }
         }
         c = strtok(NULL, " ");
-        if (count != 0 && pcount < MAX_PSIZE) {
-            command->params[pcount] = c;
-            pcount++;
-        }
 
-        if (pcount > MAX_PSIZE) {
+        if (pcount > MAX_PSIZE) { // impossible to reach?
             fprintf(stderr, "max param size reached (%d)\n", MAX_PSIZE);
             exit(1);
         }
@@ -47,7 +62,6 @@ cmd_t* tokenize_str(char* fstr) {
     command->params = p;
     command->params_size = pcount;
     return command;
-    //parse_token(c);
 }
 
 char* fmt_str(char buf[512]) {
@@ -79,7 +93,6 @@ char* fmt_str(char buf[512]) {
     }
     nb[strlen(nb)] = 0;
     nb[strlen(nb) - 1] = '\0';
-    printf("nb:%s\n", nb);
     return nb;
 }
 
@@ -97,31 +110,45 @@ int main(void) {
             fprintf(stderr, "%s\n", "error in fgets");
             return 1;
         }
+        if (strlen(buf) > MAX_LINE) {
+            fprintf(stderr, "max line length reached (%d)\n", MAX_LINE);
+            continue;
+        }
+        if (buf[0] == '\n') {
+            continue;
+        }
 
         char* fstr = fmt_str(buf); // check NULL?
+        //sscanf(fstr, "%s", fstr); ?
+        if (fstr == NULL) {
+            continue;
+        }
         if (strcmp(fstr, "quit") == 0) {
+            run_flag = 0;
             break;
         }
 
-        /*cmd_t* command = tokenize_str(fstr);
+        cmd_t* command = tokenize_str(fstr);
         if (command == NULL) {
-            fprintf(stderr, "command is null");
+            fprintf(stderr, "command is null\n");
             return 1;
         }
-        printf(
+        /*printf(
             "cmd size: %lu\n cmd: %s\n params size: %d\n", 
             sizeof(command), 
             command->cmd, 
             command->params_size
         );*/
+        parse_cmd(command);
 
-
+        free(command->cmd);
+        free(command->params);
+        free(command);
         // 1 - parse/read input
         // 2 - fork()
         // 3 - child process invokes execlp()
         // 4 - check command for &, to determine if parent should wait()
         //      (& -> no wait() , no & -> wait())
-        //break;
     }
     return 0;
 }
